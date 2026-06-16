9d186f743fea2a8fb7ed95c532e67d25ad8b0ad6
dsfsfasdfasdfasdfag-star <dsfsfasdfasdfasdfag@gmail.com>
2026-06-16 19:54:40 +0500

    Added PostgreSQL support

--- core/settings.py
+++ core/settings.py
@@ -53,8 +53,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'news_portal',
+        'USER': 'postgres',
+        'PASSWORD': 'NewPassword123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
